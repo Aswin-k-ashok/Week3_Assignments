0440259e6d0842d477eb9eb3acce2e1ddbee0352
aswin <aswinkashok0@gmail.com>
2021-10-30 16:53:20 +0530

    assignment2

--- Assignments.js
+++ Assignments.js
@@ -1,2 +1,7 @@
-var str = prompt("enter some characters");
-document.write(str);
+
+num1 =parseInt(prompt("enter the first number"));
+num2 =parseInt(prompt("enter the second number"));
+
+var sum =num1+num2;
+
+document.write("sum of ",num1," and ",num2," is ", sum);
