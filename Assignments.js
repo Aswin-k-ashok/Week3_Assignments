57383c9a010bbb5c0bad0df1b140c5f5beaf01d2
aswin <aswinkashok0@gmail.com>
2021-10-31 18:35:53 +0530

    assignment10

--- Assignments.js
+++ Assignments.js
@@ -1,16 +1,25 @@
-var pat=' ';
+const r = require('readline-sync');
+var limit = r.questionInt("enter the array size: ");
+var ary1 = [];
+var ary2 = [];
 
-for(var i=1;i<=5;i++){
-    for(var j=1;j<=i;j++){
-        pat+= j;
-    }
-    console.log(pat);
-    pat=' ';
+for(var i=0;i<limit;i++){
+    ary1.push(r.questionInt("enter the 1st array elements "));
 }
 
-// for(var i=1;i<=5;i++){
-//     for(j=1;j<=i;j++){
-//         document.write(j);
-//     }
-//     document.write("<br>");
-// }
+console.log(" ");
+
+for(var i=0;i<limit;i++){
+    ary2.push(r.questionInt("enter the 2nd array elements "));
+}
+
+var temp;
+
+for(var i=0;i<limit;i++){
+    temp=ary1[i];
+    ary1[i]=ary2[i];
+    ary2[i]=temp;
+}
+
+console.log("array 1 = ",ary1);
+console.log("array 2 = ",ary2);
