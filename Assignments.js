858eaa70730faf25c73ebce5f99d5b490216e667
aswin <aswinkashok0@gmail.com>
2021-10-31 13:25:55 +0530

    assignment5

--- Assignments.js
+++ Assignments.js
@@ -1,10 +1,26 @@
-var r = require("readline-sync");
+const r = require("readline-sync");
 
-var mark= r.questionInt("enter your mark: ");
+var mark= r.questionInt("enter your mark percentage: ");
 
-if (mark>50){
-    console.log("you have passed");
+if (mark>100){
+    console.log("enter a valid mark");
+}
+else if(mark>89){
+    console.log("A grade");
+}
+else if(mark>79){
+    console.log("B grade");
+}
+else if(mark>69){
+    console.log("C grade");
+}
+else if(mark>59){
+    console.log("D grade");
+}
+else if(mark>49){
+    console.log("E grade");
 }
 else{
     console.log("you have failed");
 }
+
