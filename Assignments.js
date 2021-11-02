935774dcb9cbfed7083d35c989e0257a37ca1ae1
aswin <aswinkashok0@gmail.com>
2021-11-02 18:48:36 +0530

    assignment18

--- Assignments.js
+++ Assignments.js
@@ -1,53 +1,9 @@
-const r = require('readline-sync');
+const r = require("readline-sync");
 
-console.log("enter 2 numbers: ");
+var wt = r.questionInt("enter the written text mark: ");
+var le = r.questionInt("enter the lab exam marks: ");
+var as = r.questionInt("enter the assignmet mark: ");
 
-var num1 = r.questionInt("");
-var num2 = r.questionInt("");
+var grade =(wt*70)/100 + (le*20)/100 + (as*10)/100;
 
-
-class result {
-
-    add(num1, num2) {
-        var res = num1 + num2;
-        return res;
-    }
-    sub(num1, num2) {
-        var res = num1 - num2;
-        return res;
-    }
-    mul(num1, num2) {
-        var res = num1 * num2;
-        return res;
-    }
-    div(num1, num2) {
-        var res = num1 / num2;
-        return res;
-    }
-}
-
-var obj = new result();
-console.log("1:addition \n2:subtraction\n3:multiplication\n4:division")
-var choice=r.questionInt("enter your choice: ");
-
-
-switch(choice){
-    case 1:
-        var sum = obj.add(num1, num2);
-        break;
-
-    case 2:
-        var sum =obj.sub(num1,num2);
-        break;
-
-    case 3:
-        var sum =obj.mul(num1,num2);
-        break;
-    case 3:
-        var sum =obj.div(num1,num2);
-        break;
-}
-
-
-
-console.log(sum);
+console.log("grade of the student: ",grade);
