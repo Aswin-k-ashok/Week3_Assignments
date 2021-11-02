69a243ae1ec2404822e211eeecd142e6ab698b3b
aswin <aswinkashok0@gmail.com>
2021-11-02 12:19:37 +0530

    assignment15

--- Assignments.js
+++ Assignments.js
@@ -1,39 +1,21 @@
-const r= require('readline-sync');
-limit = r.questionInt("enter the array size: ");
+const r = require('readline-sync');
+
+var arry=[];
+arry=getArray(arry);
+
+displayArray(arry);
 
 
-
-var arry1=[];
-var arry2=[];
-var sum=[];
-var disp="";
-
-console.log ("enter the values of the first array: ");
-
-for(var i=0;i<limit;i++){
-    arry1[i] = [];
-    for(var j=0;j<limit;j++){
-        arry1[i][j]= r.questionInt(" ");
-    }
-}
-
-console.log("enter the values of the second array: ");
-
-for(var i=0;i<limit;i++){
-    arry2[i]=[];
-    for(var j=0;j<limit;j++){
-        arry2[i][j]=r.questionInt(" ");
-    }
-}
-
-for(var i=0;i<limit;i++){
-    sum[i]=[];
-    for(var j=0;j<limit;j++){
-        sum[i][j] = arry1[i][j]+arry2[i][j];
+function getArray(arry){
+    var lmt= r.questionInt("enter the array size: ");
+    console.log("enter the array elements: ");
+    for(var i=0;i<lmt;i++){
+        arry.push(r.questionInt(" "));
         
     }
+    return arry;
 }
-
-
-console.log(sum);
-
+ 
+function displayArray(arry){
+    console.log(arry);
+}
