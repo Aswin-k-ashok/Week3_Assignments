7ad3650f2512bd6006d7cc8c4d2aa2b4d34b2e0b
aswin <aswinkashok0@gmail.com>
2021-11-03 15:33:23 +0530

    assignment25

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,62 +1,27 @@
-const r = require('readline-sync');
+var library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }
+];
 
+for(x in library){
+    if(library[x].readingStatus==true){
+        console.log('title:'+library[x].title+' '+'Author: '+library[x].author);
+    }    
+    if(library[x].readingStatus==false){
+        console.log('title:'+library[x].title+' '+'Author: '+library[x].author);
+    }
 
-class Area{
-    circle(r){
-        let area = 3.14*r*r;
-        console.log(area);
-    }
-    square(a){
-        let area = 4*a;
-        console.log(area);
-    }
-    rectangle(l,b){
-        let area = l*b;
-        console.log(area);
-    }
-    triangle(b,h){
-        let area = b*h/2;
-        console.log(area);
-    }
 }
-
-class MyClass extends Area{
-    main(){
-        console.log("1: circle\n2: square\n3: rectangle\n4: triangle");
-        var choice = r.questionInt("enter a choice: ");
-
-        switch(choice){
-            case 1:
-                let rad=r.questionInt("enter the radius: ");
-                obj.circle(rad);
-            break;
-
-            case 2:
-                let sid=r.questionInt("enter the length: ");
-                obj.square(sid);
-            break;
-
-            case 3:
-                let len=r.questionInt("enter the length: ");
-                let bre=r.questionInt("enter the bredth: ");
-                obj.rectangle(len,bre);
-            break;
-            
-            case 4:
-                let hei=r.questionInt("enter the height: ");
-                let bas=r.questionInt("enter the base: ");
-                obj.triangle(hei,bas);
-            break;
-
-
-        }
-        
-    }
-}
-
-let obj = new MyClass;
-
-obj.main();
-
-
-
