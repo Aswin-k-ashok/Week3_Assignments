76adf18af126f05020e362d54a445aefd83d6565
aswin <aswinkashok0@gmail.com>
2021-11-03 11:07:23 +0530

    assignment22

--- Assignments.js
+++ Assignments.js
@@ -1,19 +1,55 @@
-const r = require('readline-sync');
-var arry1=[];
-var arry2=[];
+// get arry()
+// add arry()
+// display arry()
 
-var limit = r.questionInt("enter the array size: ");
- 
-console.log("enter the array elements: ");
+const r =require('readline-sync');
+var arry1 = [];
+var arry2 = [];
+var sum=[];
+var size=r.questionInt("enter the array size: ");
 
-for(var i=0;i<limit;i++){
-    arry1.push(r.questionInt(" "))
+console.log("enter the 1st array elements: ");
+arry1=getArray(arry1,size);
+console.log("enter the 2nd array elements: ");
+arry2=getArray(arry2,size);
+
+sum=addArry(arry1,arry2,size);
+
+console.log("sum of the arrays:")
+
+displayArray(sum,size);
+
+
+
+function getArray(arry,size){
+    for(var i=0;i<size;i++){
+        arry[i]=[];
+        for(var j=0;j<size;j++){
+            arry[i][j]=r.questionInt(" ")
+        }
+    }
+    return arry;
 }
 
-for(var i=1,j=i+1;i<limit;i++,j++){
-    arry2.push(i*j);
+
+
+
+function addArry(arry1 ,arry2,size){
+    for(var i=0;i<size;i++){
+        sum[i]=[];
+        for(var j=0;j<size;j++){
+            sum[i][j]=arry1[i][j]+arry2[i][j];
+        }
+    }
+    return sum;
 }
 
-console.log(arry2);
-
+function displayArray(sum,size){
+    for(var i=0;i<size;i++){
+        for(var j=0;j<size;j++){
+            process.stdout.write(" "+sum[i][j]);
+        }
+        console.log();
+    }
+}
 
